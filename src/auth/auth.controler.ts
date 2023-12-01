474a32a06f11f5653e65ff687b223cdfb92901a4
greggy <sudakovgrigory@gmail.com>
2023-12-01 17:46:09 +0700

    firs working example xD

--- src/auth/auth.controler.ts
+++ src/auth/auth.controler.ts
@@ -1,5 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
+@Controller('auth')
+export class AuthControler {
+    constructor(private authService: AuthService) {}
 
-@Controller({})
-export class AuthControler{}
+    @Post('signup')
+    signup() {
+        return 'I am signed up'
+    }
+
+    @Post('signin')
+    signin() {
+        return 'I am signin up'
+    }
+}
